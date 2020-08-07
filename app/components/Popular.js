b788624f5f33fea90e4b8c4fbbb0a433ccf2305a
Mahmoud Ismail <m.ismail@speero.net>
2020-08-08 00:04:12 +0200

    added state to Popular component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,16 +1,30 @@
 import React from 'react'
 
 export default class Popular extends React.Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      selectedLanguage: 'All'
+    }
+  }
+
+  updateLanguage(selectedLanguage){
+    this.setState({selectedLanguage})
+  }
   render(){
     const languages = ['All', 'JavaScript', 'Ruby', 'CSS', 'Python'];
     return (
-      <ul className='flex-center'>
-        {languages.map(lang => (
-          <li key={lang}>
-            <button className='btn-clear nav-link'>{lang}</button>
-          </li>
-        ))}
-      </ul>
+      <React.Fragment>
+        <ul className='flex-center'>
+          {languages.map(lang => (
+            <li key={lang}>
+              <button style={this.state.selectedLanguage === lang ? {color: 'rgb(187, 46, 31)'} : null} onClick={() => this.updateLanguage(lang)} className='btn-clear nav-link'>{lang}</button>
+            </li>
+          ))}
+        </ul>
+        <h1>{this.state.selectedLanguage}</h1>
+      </React.Fragment>
     )
   }
 }
